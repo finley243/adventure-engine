84b6521f2ceb550f444219f3af8c079f09d9696d
Finley Burch <finley243@gmail.com>
2021-08-15 22:21:10 -0700

    Further develop quest system

--- src/com/github/finley243/adventureengine/quest/Objective.java
+++ src/com/github/finley243/adventureengine/quest/Objective.java
@@ -1,15 +1,33 @@
 package com.github.finley243.adventureengine.quest;
 
+import com.github.finley243.adventureengine.Game;
+import com.github.finley243.adventureengine.event.RenderTextEvent;
+
 public class Objective {
 
+	private boolean isEnabled;
 	private String prompt;
 	
 	public Objective(String prompt) {
+		this.isEnabled = false;
 		this.prompt = prompt;
 	}
 	
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
 	public String getPrompt() {
 		return prompt;
+	}
+
+	public void setEnabled(boolean state) {
+		if(state) {
+			isEnabled = true;
+			Game.EVENT_BUS.post(new RenderTextEvent("Objective - " + prompt));
+		} else {
+			isEnabled = false;
+		}
 	}
 
 }
--- src/com/github/finley243/adventureengine/quest/Quest.java
+++ src/com/github/finley243/adventureengine/quest/Quest.java
@@ -4,10 +4,16 @@ import java.util.Map;
 
 public class Quest {
 
+	private String name;
 	private Map<String, Objective> objectives;
 	
-	public Quest(Map<String, Objective> objectives) {
+	public Quest(String name, Map<String, Objective> objectives) {
+		this.name = name;
 		this.objectives = objectives;
+	}
+
+	public String getName() {
+		return name;
 	}
 	
 	public Objective getObjective(String key) {
