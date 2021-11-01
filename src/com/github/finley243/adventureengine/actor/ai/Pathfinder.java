f84827bc89b0dde927b4d6b9fa4bc352646cb671
Finley Burch <finley243@gmail.com>
2021-11-01 13:55:31 -0700

    Clean up pathfinder class

--- src/com/github/finley243/adventureengine/actor/ai/Pathfinder.java
+++ src/com/github/finley243/adventureengine/actor/ai/Pathfinder.java
@@ -16,67 +16,21 @@ public class Pathfinder {
 	 * @return The distance from one Area to the other
 	 */
 	public static int getDistance(Area area1, Area area2) {
-		return findPath(area1, area2).size() - 1;
+		List<Area> path = findPath(area1, area2);
+		if(path == null) {
+			return -1;
+		}
+		return path.size() - 1;
 	}
-	
+
 	/**
 	 * Returns a list of Areas that represents the shortest path between them
-	 * @param currentArea Start position to path from
+	 * @param startArea Start position to path from
 	 * @param targetArea Position the path leads to
 	 * @return Shortest path from currentArea to targetArea
 	 */
-	public static List<Area> findPath(Area currentArea, Area targetArea) {
-		/*
-		Set<Area> visited = new HashSet<Area>();
-		return findPath(currentArea, targetArea, visited, -1);
-		*/
-		return findPathBFS(currentArea, targetArea);
-	}
-	
-	// sizeAllowed: length allowed for sub-path (if -1, any length is allowed), based on current shortest path
-	private static List<Area> findPath(Area currentArea, Area targetArea, Set<Area> hasVisited, int sizeAllowed) {
-		//System.out.println("findPath(" + currentArea + ", " + targetArea + ", " + hasVisited + ")");
-		if(currentArea == targetArea) {
-			List<Area> path = new ArrayList<Area>();
-			path.add(0, currentArea);
-			return path;
-		} else if(sizeAllowed == 0) {
-			return null;
-		}
-		hasVisited.add(currentArea);
-		Set<Area> linkedAreasGlobal = new HashSet<Area>();
-		linkedAreasGlobal.addAll(currentArea.getLinkedAreas());
-		if(currentArea.getRoom() != targetArea.getRoom()) {
-			for(WorldObject object : currentArea.getObjects()) {
-				if(object instanceof ObjectExit) {
-					linkedAreasGlobal.add(((ObjectExit) object).getLinkedArea());
-				} else if(object instanceof ObjectElevator) {
-					linkedAreasGlobal.addAll(((ObjectElevator) object).getLinkedAreas());
-				}
-			}
-		}
-		if(hasVisited.containsAll(linkedAreasGlobal)) {
-			return null;
-		}
-		List<Area> shortestPath = null;
-		for(Area linkedArea : linkedAreasGlobal) {
-			if(!hasVisited.contains(linkedArea)) {
-				List<Area> subPath = findPath(linkedArea, targetArea, hasVisited, (sizeAllowed == -1 ? -1 : sizeAllowed - 1));
-				if(subPath != null) {
-					subPath.add(0, currentArea);
-					if(shortestPath == null || subPath.size() < shortestPath.size()) {
-						shortestPath = subPath;
-						sizeAllowed = subPath.size();
-					}
-				}
-			}
-		}
-		hasVisited.remove(currentArea);
-		return shortestPath;
-	}
-
-	private static List<Area> findPathBFS(Area startArea, Area targetArea) {
-		Set<Area> hasVisited = new HashSet<Area>();
+	private static List<Area> findPath(Area startArea, Area targetArea) {
+		Set<Area> hasVisited = new HashSet<>();
 		Queue<List<Area>> paths = new LinkedList<>();
 		List<Area> startPath = new ArrayList<>();
 		startPath.add(startArea);
