5672b1e0d1201be98dbf72925201c80bc7f35beb
Finley Burch <finley243@gmail.com>
2022-03-08 11:48:44 -0800

    Add network action framework to SubNet

--- src/com/github/finley243/adventureengine/network/SubNetwork.java
+++ src/com/github/finley243/adventureengine/network/SubNetwork.java
@@ -1,22 +1,42 @@
 package com.github.finley243.adventureengine.network;
 
+import com.github.finley243.adventureengine.action.Action;
+import com.github.finley243.adventureengine.actor.Actor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SubNetwork {
 
+    private final String name;
     private final List<String> connected;
     private final int securityLevel;
+    private final List<SubNetwork> subNets;
 
     private boolean isBreached;
 
-    public SubNetwork(int securityLevel, List<String> connected) {
+    public SubNetwork(String name, int securityLevel, List<String> connected, List<SubNetwork> subNets) {
+        this.name = name;
         this.connected = connected;
         this.securityLevel = securityLevel;
+        this.subNets = subNets;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean isBreached() {
         return isBreached;
+    }
+
+    public List<Action> networkActions(Actor subject) {
+        List<Action> actions = new ArrayList<>();
+
+        for(SubNetwork subNet : subNets) {
+            actions.addAll(subNet.networkActions(subject));
+        }
+        return actions;
     }
 
 }
