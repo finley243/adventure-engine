b0ac219ae1169044cff78d85ab3463781984b215
Finley Burch <finley243@gmail.com>
2022-03-31 22:07:31 -0700

    Add effect stacking to EffectComponent (currently allowed for all effects)

--- src/com/github/finley243/adventureengine/actor/component/EffectComponent.java
+++ src/com/github/finley243/adventureengine/actor/component/EffectComponent.java
@@ -9,7 +9,7 @@ public class EffectComponent {
 
     private final Actor actor;
     // Integer value is number of turns effect has been active (if effect does not require manual removal)
-    private final Map<Effect, Integer> effects;
+    private final Map<Effect, List<Integer>> effects;
 
     public EffectComponent(Actor actor) {
         effects = new HashMap<>();
@@ -19,14 +19,20 @@ public class EffectComponent {
     public void addEffect(Effect effect) {
         effect.start(actor);
         if(!effect.isInstant()) {
-            effects.put(effect, 0);
+            if(!effects.containsKey(effect)) {
+                effects.put(effect, new ArrayList<>());
+            }
+            effects.get(effect).add(0);
         }
     }
 
     public void removeEffect(Effect effect) {
         if(effects.containsKey(effect)) {
             effect.end(actor);
-            effects.remove(effect);
+            effects.get(effect).remove(0);
+            if(effects.get(effect).isEmpty()) {
+                effects.remove(effect);
+            }
         }
     }
 
@@ -35,11 +41,17 @@ public class EffectComponent {
         while(itr.hasNext()) {
             Effect effect = itr.next();
             effect.eachTurn(actor);
-            int counterValue = effects.get(effect) + 1;
-            effects.put(effect, counterValue);
-            if(!effect.manualRemoval() && counterValue == effect.getDuration()) {
-                effect.end(actor);
-                itr.remove();
+            List<Integer> counters = effects.get(effect);
+            for(int i = 0; i < counters.size(); i++) {
+                int counterValue = counters.get(i) + 1;
+                counters.set(i, counterValue);
+                if(!effect.manualRemoval() && counterValue == effect.getDuration()) {
+                    effect.end(actor);
+                    counters.remove(0);
+                    if(counters.isEmpty()) {
+                        itr.remove();
+                    }
+                }
             }
         }
     }
