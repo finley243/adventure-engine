93feb5c4e221fbc53bd83fd54bc09e1c3f2272ae
Finley Burch <finley243@gmail.com>
2022-06-18 17:26:56 -0700

    Add AreaEffect object (functionality not implemented)

---
+++ src/com/github/finley243/adventureengine/effect/AreaEffect.java
@@ -0,0 +1,19 @@
+package com.github.finley243.adventureengine.effect;
+
+import java.util.List;
+
+public class AreaEffect {
+
+    private final int duration;
+    private final List<Effect> effects;
+
+    public AreaEffect(int duration, List<Effect> effects) {
+        this.duration = duration;
+        this.effects = effects;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+}
