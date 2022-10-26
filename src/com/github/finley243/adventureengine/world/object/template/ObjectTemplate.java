a5b1c72a5be9a941fe972b7ec011faf99f11668f
Finley Burch <finley243@gmail.com>
2022-10-26 16:44:22 -0700

    Add scripts and custom actions to ObjectTemplate

--- src/com/github/finley243/adventureengine/world/object/template/ObjectTemplate.java
+++ src/com/github/finley243/adventureengine/world/object/template/ObjectTemplate.java
@@ -1,17 +1,26 @@
 package com.github.finley243.adventureengine.world.object.template;
 
+import com.github.finley243.adventureengine.action.ActionCustom;
 import com.github.finley243.adventureengine.scene.Scene;
+import com.github.finley243.adventureengine.script.Script;
+
+import java.util.List;
+import java.util.Map;
 
 public class ObjectTemplate {
 
     private final String ID;
     private final String name;
     private final Scene description;
+    private final Map<String, Script> scripts;
+    private final List<ActionCustom> customActions;
 
-    public ObjectTemplate(String ID, String name, Scene description) {
+    public ObjectTemplate(String ID, String name, Scene description, Map<String, Script> scripts, List<ActionCustom> customActions) {
         this.ID = ID;
         this.name = name;
         this.description = description;
+        this.scripts = scripts;
+        this.customActions = customActions;
     }
 
     public String getID() {
@@ -24,6 +33,14 @@ public class ObjectTemplate {
 
     public Scene getDescription() {
         return description;
+    }
+
+    public Map<String, Script> getScripts() {
+        return scripts;
+    }
+
+    public List<ActionCustom> getCustomActions() {
+        return customActions;
     }
 
 }
