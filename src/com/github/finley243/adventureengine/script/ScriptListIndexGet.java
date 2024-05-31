94d5d0fe3fd42b302c3804bc3e61ca443ddb9efc
Finley Burch <finley243@gmail.com>
2024-05-31 15:33:42 -0700

    Add native functions to get/set list elements by index

---
+++ src/com/github/finley243/adventureengine/script/ScriptListIndexGet.java
@@ -0,0 +1,23 @@
+package com.github.finley243.adventureengine.script;
+
+import com.github.finley243.adventureengine.Context;
+import com.github.finley243.adventureengine.expression.Expression;
+
+import java.util.List;
+
+public class ScriptListIndexGet extends Script {
+
+    @Override
+    public ScriptReturnData execute(Context context) {
+        Expression listExpression = context.getLocalVariables().get("list").getExpression();
+        Expression indexExpression = context.getLocalVariables().get("index").getExpression();
+        if (listExpression.getDataType() != Expression.DataType.LIST) return new ScriptReturnData(null, null, "List parameter is not a list");
+        if (indexExpression.getDataType() != Expression.DataType.INTEGER) return new ScriptReturnData(null, null, "Index parameter is not an integer");
+        List<Expression> list = listExpression.getValueList();
+        int index = indexExpression.getValueInteger();
+        if (index < 0 || index > list.size()) return new ScriptReturnData(null, null, "Index is out of bounds");
+        Expression valueAtIndex = list.get(index);
+        return new ScriptReturnData(valueAtIndex, null, null);
+    }
+
+}
