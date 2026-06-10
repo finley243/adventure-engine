aaceb4d243d0f5cdcaf801b7c3dddb4629762382
finley243 <finley243@gmail.com>
2026-06-09 17:06:32 -0700

    Add ability to specify functions to execute on timer expire and update through ScriptTimerStart

--- src/main/java/com/github/finley243/adventureengine/script/ScriptTimerStart.java
+++ src/main/java/com/github/finley243/adventureengine/script/ScriptTimerStart.java
@@ -14,10 +14,23 @@ public class ScriptTimerStart extends Script {
     public ScriptReturnData execute(Context context) {
         Expression timerID = context.getLocalVariables().get("timer").getExpression();
         Expression timerDuration = context.getLocalVariables().get("duration").getExpression();
+        Expression timerScriptExpire = context.getLocalVariables().get("scriptExpire").getExpression();
+        Expression timerScriptUpdate = context.getLocalVariables().get("scriptUpdate").getExpression();
         if (timerID.getDataType() != Expression.DataType.STRING) return new ScriptReturnData(null, null, new ScriptErrorData("ScriptTimerStart timerID is not a string", getTraceData()));
         if (timerDuration.getDataType() != Expression.DataType.INTEGER) return new ScriptReturnData(null, null, new ScriptErrorData("ScriptTimerStart timerDuration is not an integer", getTraceData()));
-        // TODO - Design a way to define delayed scripts through timers in new script parser system
-        Timer timer = new Timer(context.game(), timerID.getValueString(), timerDuration.getValueInteger(), null, null, context);
+        if (timerScriptExpire != null && timerScriptExpire.getDataType() != Expression.DataType.STRING) return new ScriptReturnData(null, null, new ScriptErrorData("ScriptTimerStart scriptExpire is not a string", getTraceData()));
+        if (timerScriptUpdate != null && timerScriptUpdate.getDataType() != Expression.DataType.STRING) return new ScriptReturnData(null, null, new ScriptErrorData("ScriptTimerStart scriptUpdate is not a string", getTraceData()));
+        Script scriptExpire = null;
+        Script scriptUpdate = null;
+        if (timerScriptExpire != null) {
+            String scriptExpireID = timerScriptExpire.getValueString();
+            scriptExpire = context.game().data().getScript(scriptExpireID).script();
+        }
+        if (timerScriptUpdate != null) {
+            String scriptUpdateID = timerScriptUpdate.getValueString();
+            scriptUpdate = context.game().data().getScript(scriptUpdateID).script();
+        }
+        Timer timer = new Timer(context.game(), timerID.getValueString(), timerDuration.getValueInteger(), scriptExpire, scriptUpdate, context);
         context.game().data().addTimer(timer.getID(), timer);
         return new ScriptReturnData(null, null, null);
     }
